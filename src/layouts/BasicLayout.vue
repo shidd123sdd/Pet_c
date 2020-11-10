b00f079db7c141dec8961d33cde5441f7ee61a3d
shi_dd <838713426@qq.com>
2020-11-10 15:09:02 +0800

    'SideNav'

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -3,60 +3,7 @@
     <a-layout id="components-layout-demo-top-side-2">
       <Header />
       <a-layout>
-        <a-layout-sider width="200" style="background: #fff">
-          <a-menu
-            mode="inline"
-            :default-selected-keys="['1']"
-            :default-open-keys="['sub1']"
-            :style="{ height: '100%', borderRight: 0 }"
-          >
-            <a-sub-menu key="sub1">
-              <span slot="title"><a-icon type="user" />subnav 1</span>
-              <a-menu-item key="1">
-                option1
-              </a-menu-item>
-              <a-menu-item key="2">
-                option2
-              </a-menu-item>
-              <a-menu-item key="3">
-                option3
-              </a-menu-item>
-              <a-menu-item key="4">
-                option4
-              </a-menu-item>
-            </a-sub-menu>
-            <a-sub-menu key="sub2">
-              <span slot="title"><a-icon type="laptop" />subnav 2</span>
-              <a-menu-item key="5">
-                option5
-              </a-menu-item>
-              <a-menu-item key="6">
-                option6
-              </a-menu-item>
-              <a-menu-item key="7">
-                option7
-              </a-menu-item>
-              <a-menu-item key="8">
-                option8
-              </a-menu-item>
-            </a-sub-menu>
-            <a-sub-menu key="sub3">
-              <span slot="title"><a-icon type="notification" />subnav 3</span>
-              <a-menu-item key="9">
-                option9
-              </a-menu-item>
-              <a-menu-item key="10">
-                option10
-              </a-menu-item>
-              <a-menu-item key="11">
-                option11
-              </a-menu-item>
-              <a-menu-item key="12">
-                option12
-              </a-menu-item>
-            </a-sub-menu>
-          </a-menu>
-        </a-layout-sider>
+        <SideNav />
         <a-layout style="padding: 0 24px 24px">
           <a-breadcrumb style="margin: 16px 0">
             <a-breadcrumb-item>Home</a-breadcrumb-item>
@@ -81,6 +28,7 @@
 
 <script>
 import Header from "./Header";
+import SideNav from "./SideNav";
 export default {
   data() {
     return {
@@ -88,7 +36,8 @@ export default {
     };
   },
   components: {
-    Header
+    Header,
+    SideNav
   }
 };
 </script>
