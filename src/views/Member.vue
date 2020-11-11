e100462527b012e00f9b45174a8610f382b4fc58
shi_dd <838713426@qq.com>
2020-11-11 11:45:36 +0800

    新增模块

---
+++ src/views/Member.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>会员</div>
+    <Table />
+  </div>
+</template>
+
+<script>
+import Table from "../components/Table";
+export default {
+  components: {
+    Table
+  }
+};
+</script>
+
+<style scoped lang="less"></style>
---
+++ src/views/Product.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>商品</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="less"></style>
